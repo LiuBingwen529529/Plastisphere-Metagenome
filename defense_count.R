76aee859320afbfa77e4a189b349e4fcdc80dfd0
LiuBingwen529529 <liubw24@mails.tsinghua.edu.cn>
2025-10-07 10:55:02 +0800

    Delete defense_count.R

--- defense_count.R
+++
@@ -1,7 +0,0 @@
-# 读取数据
-VHR<-read.csv("/Users/lbw/Desktop/defense_VHR.csv",head=T,stringsAsFactors=F)
-defense<-read.csv("/Users/lbw/Desktop/defense_system_count.csv",head=T,stringsAsFactors=F)
-library(dplyr)
-result <- VHR %>%
-  left_join(defense, by = "MAG") 
-write.csv(result, file = "/Users/lbw/Desktop/defense_system_VHR.csv")
